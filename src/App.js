561441bebb45115ecf3278250eff0a1f7ba4603d
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-09-06 00:58:49 -0700

    add location state

--- src/App.js
+++ src/App.js
@@ -2,43 +2,18 @@ import Dashboard from "./components/weatherdashbord/Dashboard";
 import DisplayDay from "./components/displaydays/DisplayDay";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import FavoriteCity from "./components/favoritcity/FavoriteCity";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+
 
 function App() {
   const [location, setLocation] = useState("Yerevan");
-  const [currentData, setCurrentData] = useState({});
-
-  const fetchinData = async (location = "Yerevan") => {
-    try {
-      await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=135&q=${location}&appid=f8e49ca944d77bb0460d19944e58e0ee`)
-        .then((res) => {
-          return res.json()
-        })
-        .then((data) => {
-          setCurrentData(data)
-        })
-    } catch {
-
-    }
-  }
-
-  useEffect(() => {
-    fetchinData()
-  }, [location])
-
-  const searchLocation = (event) => {
-    if (event.key === "Enter") {
-      fetchinData(location)
-    }
-  }
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard location={location} setLocation={setLocation} currentData={currentData}
-                                              searchLocation={searchLocation} setCurrentData={setCurrentData}/>}/>
+          <Route path="/" element={<Dashboard location={location} setLocation={setLocation}/>}/>
           <Route path="/fivedaysinfo" element={<DisplayDay location={location}/>}/>
-          <Route path="/favoriteCities" element={<FavoriteCity/>}/>
+          <Route path="/favoriteCities" element={<FavoriteCity location={location}/>}/>
         </Routes>
       </BrowserRouter>
     </div>
